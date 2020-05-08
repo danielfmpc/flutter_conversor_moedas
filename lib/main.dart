4943418d1524b92b5ba142eba3bb28aa50b11c88
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-05-08 03:59:30 -0300

    Adicionando theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,17 @@ void main(){
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
+      theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )
+      ),
     ),
   );
 }
